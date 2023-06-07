1c9163e465602d14052db8305ee148991ec77e2b
Tim Godin <tim_godin@msn.com>
2023-06-07 13:21:36 -0600

    minor adjust

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,15 @@
     <h1>Bonnie Godin</h1>
 </header>
 
-<br>
+<nav class="flex">
+    <div class="flex-auto"><a href="/">home</a></div>
+    <div class="flex-auto"><a href="/about">about</a></div>
+    <div class="flex-auto"><a href="/gallery">gallery</a></div>
+    <div class="flex-auto"><a href="/blog">blog</a></div>
+    <div class="flex-auto"><a href="/contact">contact</a></div>
+</nav>
 
+<br>
 <slot></slot>
 
 <br>
@@ -18,11 +25,17 @@
     <br>
     <!-- i want a box to sign up for email newsletter here -->
     <div class="newsletter">
-        <h3>Sign up for my newsletter!</h3>
-        <form action="https://formspree.io/f/maylqzqk" method="POST">
-            <input type="email" name="_replyto" placeholder="Your email" />
-            <button type="submit">Send</button>
-        </form>
+        <div class="flex-auto">
+            <h3>Sign up for my newsletter!</h3>
+        </div>
+
+        <div class="flex-auto">
+            <form action="https://formspree.io/f/maylqzqk" method="POST">
+                <input type="email" name="_replyto" placeholder="Your email" />
+                <button type="submit">Send</button>
+            </form>
+        </div>
+        
     </div>
    
     <!-- I want a basic nav here to go between HOME ABOUT GALLERY and CONTACT -->
@@ -40,6 +53,13 @@
 
 
 <style>
+    hr {
+
+        /* make it bolder */
+        border-top: 3px solid black;
+        /* round off the sides */
+        border-radius: 5px;
+    }
     /* make the nav flex horizontally and space evenly */
     ul {
         display: flex;
@@ -53,8 +73,9 @@
     header {
         text-align: center;
         font-size: 3rem;
-        /* give it a black inner shadow */
-        text-shadow: 0 0 0.5rem white;
+        margin-bottom: 0;
+        padding-bottom: 0; 
+        margin-top: 0;       
     }
     br {
         margin-bottom: 1rem;
@@ -64,9 +85,35 @@
     .newsletter {
         text-align: center;
         font-size: 0.8rem;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
     }
     form {
         display: flex;
         justify-content: center;
+    }
+    /* tighten up the nav - smaller and less space, center everything tightly  */
+    nav {
+        display: flex;
+        justify-content: center;
+        font-size: 0.8rem;
+        /* all caps */
+        text-transform: uppercase;
+        font-weight: bold;
+    }
+    /* for the nav items - less space around each and a border around each */
+    nav a {
+        padding: 0.5rem;  
+        color: black      
+    }
+    h1 {
+        margin-bottom: 0;
+        margin-top: 0;
+        padding-bottom: 0;
+        /* strong black outline */
+        text-shadow: 2px 2px 0px #000,
+                    -2px 2px 0px #000,
+                    2px -2px 0px #000,
+                    -2px -2px 0px #000;
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,27 @@
 <!-- making a generic portfolio landing -->
 <!-- needs gallery and about links -->
-<h1>Bonnie Godin</h1>
+<script>
+    import Splash from '../components/Splash.svelte'
+</script>
 
-<p>Hey! I'm Bonnie. I'm an emerging Canadian artist from Calgary, AB with a passion for vibrant colours. My hope is that my art will connect me with those who are choosing creativity and a light heart in the face of their own stories.</p>
+<Splash />
+
+<div class="flex">
+
+    <div class="flex-auto">
+        <p>
+            Hey! I'm Bonnie. I'm an emerging Canadian artist from Calgary, AB 
+            with a passion for vibrant colours. My hope is that my art will 
+            connect me with those who are choosing creativity and a light heart 
+            in the face of their own stories.
+        </p>
+    </div>
+
+
+</div>
+
+<style>
+    
+
+
+</style>
