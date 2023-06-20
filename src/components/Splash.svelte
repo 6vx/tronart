07c938c42fbccb0fa7ee409e7b15fa54e864db68
Tim Godin <tim_godin@msn.com>
2023-06-19 22:23:23 -0600

    fade fail

--- src/components/Splash.svelte
+++ src/components/Splash.svelte
@@ -2,6 +2,8 @@
     // i want to cycle the splash between three images, and use a fade transition for it
     import { Carousel, CarouselTransition } from 'flowbite-svelte'
 
+    import { crossfade } from 'svelte/transition';
+
     let images = [
         {
     id: 0,
@@ -23,11 +25,46 @@
   },
     ];
 
+    let currentImage = 0;
+
+    function changeImage() {
+      currentImage = (currentImage + 1) % images.length;
+    }
+
+    // change the image every five seconds
+    setInterval(changeImage, 5000);
+
 
 
     
     
 </script>
+
+<div class="splashHolder">
+  <div class="splash">
+
+  </div>
+
+  {#if currentImage === 0}
+    <div class="splash2" style="background-image: url('/tron1.jpg');">
+    </div>
+{/if}
+
+
+{#if currentImage === 1}
+    <div class="splash2" style="background-image: url('/tron2.jpg');">
+    </div>
+{/if}
+
+{#if currentImage === 2}
+    <div class="splash2" style="background-image: url('/tron3.jpg');">
+    </div>
+{/if}
+
+
+</div>
+
+
 
 
 
@@ -37,22 +74,39 @@
 
 <style>
 
-    /* .splash {
-    background-image: url('/tron1.jpg');
+    .splash {
+    position: absolute;
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
     height: 55vh;
     margin: 0px;
     padding: 0px;
-    } */
+    }
 
-    .splash {
+    /* .splash {
         height: 55vh;
+        width: 100%;
         margin: 0px;
         padding: 0px;
         overflow: hidden;
-        max-width: 100%;
+    } */
+
+    /* splash2 will float over top of splash */
+    .splash2 {
+        height: 55vh;
+        width: 100%;
+        margin: 0px;
+        padding: 0px;
+        overflow: hidden;
+        position: relative;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        top: 0px;
+        left: 0px;
+        opacity: 1;
+        transition: opacity 1s ease-in-out;
     }
 
 </style>
