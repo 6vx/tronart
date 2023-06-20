3cf94c87894fb1680d400ba6cc3921ecf9ce6481
Tim Godin <tim_godin@msn.com>
2023-06-19 22:11:39 -0600

    remove carousel for now

--- src/components/Splash.svelte
+++ src/components/Splash.svelte
@@ -1,51 +1,36 @@
 <script>
     // i want to cycle the splash between three images, and use a fade transition for it
+    import { Carousel, CarouselTransition } from 'flowbite-svelte'
 
-    import { fade } from 'svelte/transition';
-    import { onMount } from 'svelte';
-
-    let index = 0;
     let images = [
-        '/tron1.jpg',
-        '/tron2.jpg',
-        '/tron3.jpg'
+        {
+    id: 0,
+    name: "Bonnie Godin",
+    imgurl: "/tron1.jpg",
+    attribution: "Bonnie Godin",
+  },
+  {
+    id: 1,
+    name: "Bonnie Godin",
+    imgurl: "/tron2.jpg",
+    attribution: "Bonnie Godin",
+  },
+  {
+    id: 2,
+    name: "Bonnie Godin",
+    imgurl: "/tron3.jpg",
+    attribution: "Bonnie Godin",
+  },
     ];
 
-    let currentImage = 0;
 
-    const nextImage = () => {
-        currentImage = (currentImage + 1) % images.length;
-    }
-
-    onMount(() => {
-        const interval = setInterval(() => {
-            nextImage();
-        }, 5000);
-
-        return () => clearInterval(interval);
-    });
 
     
     
 </script>
 
-<div class="splash">
-    {#if currentImage == 0}
-    <div class="image1" out:fade="{{duration:400}}" in:fade="{{delay:405, duration:700}}">
-        <img src={images[0]} alt="tron1" />
-    </div>
-    {/if}
-    {#if currentImage == 1}
-    <div class="image1" out:fade="{{duration:400}}" in:fade="{{delay:405, duration:700}}">
-        <img src={images[1]} alt="tron2" />
-    </div>
-    {/if}
-    {#if currentImage == 2}
-    <div class="image1" out:fade="{{duration:400}}" in:fade="{{delay:405, duration:700}}">
-        <img src={images[2]} alt="tron3" />
-    </div>
-    {/if}
-</div>
+
+
 
 
 
@@ -66,21 +51,8 @@
         height: 55vh;
         margin: 0px;
         padding: 0px;
-    }
-
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-    /* float image1 above the splash with the same height */
-    .image1 {
-        position: relative;
-        top: 0;
-        left: 0;
-        height: 55vh;
-        width: 100%;
-        z-index: 1;
+        overflow: hidden;
+        max-width: 100%;
     }
 
 </style>
