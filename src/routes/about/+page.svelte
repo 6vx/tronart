815a5326bbc8a3218d8e3588c851dd3cbc72fc25
Tim Godin <tim_godin@msn.com>
2023-07-04 12:29:25 -0600

    about page photo added

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,7 +4,9 @@
 
 <div class="flex">
     <div class="flex-auto">
-        <!-- <h1>About</h1> -->
+        <img class="aboutImage" src="/bonnieAboutPage.JPG" alt="">
+    </div>
+    <div class="flex-auto">
         <Bio />
     </div>
 </div>
@@ -17,3 +19,10 @@
 <p>An artist as far back as she can remember, historically focusing on graphite pencil and charcoal, finally begun her journey with oil painting beginning early 2022.</p>
 
 <p>Check out the portfolio section to view student assignments and passion projects.</p> -->
+
+<style>
+    /* i want aboutImage to be the width of the screen */
+    .aboutImage {
+        width: 100%;
+    }
+</style>
