f457f8b53967ae146ffda5e94deca21d25736c5c
Tim Godin <tim_godin@msn.com>
2023-07-04 14:11:09 -0600

    checking on mobile

--- src/routes/rockymountainanimals/+page.svelte
+++ src/routes/rockymountainanimals/+page.svelte
@@ -1,3 +1,44 @@
-<h1>Rocky Mountain Animals</h1>
+<script>
+    // import bonnie.ts
+    import { artPieces } from '../../bonnie.ts'
 
-<!-- I'll be displaying all the art pieces from  -->
+   let rockyMountainAnimals = []
+
+    artPieces.forEach((artPiece) => {
+        if (artPiece.belongsToCollection != "Rocky Mountain Animals") {
+            return
+        } else {
+            rockyMountainAnimals.push(artPiece)
+        }
+    })
+
+    console.log(artPieces)
+
+</script>
+
+<div class="flex">
+    <div class="flex-auto">
+    <h1>Rocky Mountain Animals</h1>
+
+
+    </div>
+
+    <div class="flex">
+        {#each rockyMountainAnimals as artPiece}
+            <div class="flex-auto">
+                <img src={artPiece.image} alt="">
+                <h3>{artPiece.title}</h3>
+                <p>{artPiece.description}</p>
+            </div>
+        {/each}
+    </div>
+
+</div>
+
+
+<style>
+    .flex {
+        text-align: center;
+    }
+    
+</style>
