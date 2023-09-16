8b1cdc1095de153886714a8dffc14a5130155103
Tim Godin <tim_godin@msn.com>
2023-09-16 15:45:57 -0600

    Added blog utility script

--- README.md
+++ README.md
@@ -1,44 +1,30 @@
+# Bonnie's Website
 
-header font is Playfair Display, Libre Baskerville
-regular font is 
-color for accents and links is 
+## Installing / Preparing
 
+In order to use this package you need to have the following installed on your machine
 
-# create-svelte
+an IDE (visual studio code)
+NODE JS 
+DENO
+GIT 
 
-Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte).
+You must be logged into your git by setting credentials in command line (git user.email --global ____ etc)
+You must install the packages associated with the repository one time (npm install)
 
-## Creating a project
+## Updating / Modifying
 
-If you're seeing this, you've probably already done this step. Congrats!
+To enter development mode you must type
 
-```bash
-# create a new project in the current directory
-npm create svelte@latest
-
-# create a new project in my-app
-npm create svelte@latest my-app
-```
-
-## Developing
-
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
-
-```bash
 npm run dev
 
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
-```
+To refresh the blog content you must type
 
-## Building
+deno run --allow-read --allow-write refreshBlog.ts
 
-To create a production version of your app:
+## Notes and more
 
-```bash
-npm run build
-```
+header font is Playfair Display, Libre Baskerville
 
-You can preview the production build with `npm run preview`.
+Check out the (Markdown Cheatsheet)[https://www.markdownguide.org/cheat-sheet]
 
-> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
---
+++ refreshBlog.ts
@@ -0,0 +1,82 @@
+// this script will refresh the blog
+// it'll be run by executing it with Deno in the root directory
+// using the following command
+
+// deno run --allow-read --allow-write refreshBlog.ts
+
+// inside the folder static/posts there are a set of markdown files
+// these files need to be parsed into html for consumption by the sveltekit app/website
+
+// the markdown files are parsed into javascript objects with the following properties:
+// - title
+// - date
+// - slug
+// - hiddentags
+// - thumbnail
+// - banner
+// - teaser
+// - content
+
+interface Post { 
+    title: string,
+    date: string,
+    slug: string,
+    hiddentags: string[],
+    thumbnail: string,
+    banner: string,
+    teaser: string,
+    content: string,
+    html: string,
+}
+
+// import the markdown parser
+import * as marked from 'npm:marked';
+
+// import frontmatter parser
+import * as mod from "https://deno.land/std@0.147.0/encoding/front_matter.ts";
+
+const postList = [];
+
+// for each markdown file in the posts folder
+// parse the markdown a Post object
+// and add it to the PostList
+
+for await (const file of Deno.readDir("./static/posts")) {
+    if (file.isFile) {
+        const post: Post = {
+            title: "",
+            date: "",
+            slug: "",
+            hiddentags: [],
+            thumbnail: "",
+            banner: "",
+            teaser: "",
+            content: "",
+            html: "",
+        };
+        const markdownFile = await Deno.readTextFile(`./static/posts/${file.name}`);
+        console.log(markdownFile);
+
+        const post_data = mod.extract(markdownFile);
+        console.log(post_data);
+
+        // the content is the post_data.body
+        post.content = post_data.body;
+        // the other attributes are in post_data.attrs object
+        post.title = post_data.attrs.title;
+        post.date = post_data.attrs.date;
+        post.slug = post_data.attrs.slug;
+        post.hiddentags = post_data.attrs.hiddentags;
+        post.thumbnail = post_data.attrs.thumbnail;
+        post.banner = post_data.attrs.banner;
+        post.teaser = post_data.attrs.teaser;
+
+        // convert the content to html from markdown
+        post.html = marked.parse(post.content);
+
+        postList.push(post);
+    }
+}
+
+// at the end we write everything to a ts file
+await Deno.writeTextFile("./static/posts.ts", JSON.stringify(postList));
